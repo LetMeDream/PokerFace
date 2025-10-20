eeb7cd8672311bc9ae5578a6b37b25b4363b3dd0
Raul <raulalfonzo66@gmail.com>
2025-10-20 12:46:02 -0400

    fix (Third): fix Third component

--- src/sections/Third/Third.tsx
+++ src/sections/Third/Third.tsx
@@ -9,7 +9,7 @@ const Third = () => {
       <div className='bg-secondary montserrat-font text-center text-[#FFB400] text-lg px-8 md:px-40 py-4 md:py-5 md:text-[26px] lg:text-3xl xl:text-[32px] xl:pt-1 xl:pb-4'>
         Los juegos que más te gustan, en un solo lugar:
       </div>
-      <div className="main-container">
+      <div className="main-third-container">
         <div className=" flex flex-col md:flex-row px-4 py-10 max-w-[1800px] mx-auto">
           <img src={ruleta} alt="ruleta" className='third-ruleta-image max-w-[340px] md:max-w-[33%] 2xl:max-w-[420px] justify-center mx-auto cursor-pointer hover:scale-105 transition duration-700' />
           <img src={slots} alt="slots" className='third-slots-image max-w-[340px] md:max-w-[33%] 2xl:max-w-[420px] justify-center mx-auto cursor-pointer hover:scale-105 transition duration-700' />
