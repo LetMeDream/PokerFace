38aa9495dfb23133b4792746365b2aec65300e99
Raul <raulalfonzo66@gmail.com>
2025-10-20 00:57:04 -0400

    feat (Fourth): create the Fourth Section

--- src/sections/Fourth/Fourth.tsx
+++ src/sections/Fourth/Fourth.tsx
@@ -1,9 +1,13 @@
 import testimonios from '../../assets/sections/fourth/testimonios.png'
+import testimoniosdesktop from '../../assets/sections/fourth/testimonios-desktop.png'
+import testimonios3220 from '../../assets/sections/fourth/testimonios-3220.png'
 
 const Fourth = () => {
   return (
-    <div>
-      <img src={testimonios} alt="testimonios" className=' scale-105' />
+    <div className=' overflow-hidden'>
+      <img src={testimonios} alt="testimonios" className=' scale-105 md:hidden' />
+      <img src={testimoniosdesktop} alt="testimonios desktop" className=' hidden md:block 2xl:hidden' />
+      <img src={testimonios3220} alt="testimonios 3220" className=' hidden 2xl:block' />
     </div>
   )
 }
