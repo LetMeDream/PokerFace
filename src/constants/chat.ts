be230ae219c17cb81c140d2825d9d28eecc896ff
Raul <raulalfonzo66@gmail.com>
2025-10-21 18:38:33 -0400

    chore: create chat constants file

---
+++ src/constants/chat.ts
@@ -0,0 +1,29 @@
+export const messages = [{
+    type: 'guest',
+    content: "Hola! Creo que necesito ayuda."
+  }, {
+    type: 'agent',
+    content: "¡Hola! ¿Cómo puedo ayudarte?"
+  }, {
+    type: 'guest',
+    content: "¡Me encanta el sitio web!"
+  },{
+    type: 'guest',
+    content: "Hola! Creo que necesito ayuda."
+  }, {
+    type: 'agent',
+    content: "¡Hola! ¿Cómo puedo ayudarte?"
+  }, {
+    type: 'guest',
+    content: "¡Me encanta el sitio web!"
+  },{
+    type: 'guest',
+    content: "Hola! Creo que necesito ayuda."
+  }, {
+    type: 'agent',
+    content: "¡Hola! ¿Cómo puedo ayudarte?"
+  }, {
+    type: 'guest',
+    content: "¡Me encanta el sitio web!"
+  }
+]
