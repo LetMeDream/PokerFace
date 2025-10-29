644fe1cf7aa08ef7f0bfd7be54303a1167f24353
Raul <raulalfonzo66@gmail.com>
2025-10-29 19:54:17 -0400

    feat (logout): create the setUsetLogout action

---
+++ src/utils/actions.ts
@@ -0,0 +1,10 @@
+import { logout } from "../store/slices/auth"
+import { clearUser } from "../store/slices/user"
+import { unsetChatProfile } from "../store/slices/chat_profile"
+import { store } from "../store/store"
+
+export const logUserOut = () => {
+  store.dispatch(logout())
+  store.dispatch(clearUser())
+  store.dispatch(unsetChatProfile())
+}
