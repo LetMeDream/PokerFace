a0c68056fa57b63dc88cd5dc8711e23359edc3ec
Raul <raulalfonzo66@gmail.com>
2025-10-29 10:06:24 -0400

    feat (store): import and use the newly created slices

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from './slices/auth'
+import { userSlice } from './slices/user'
+import { chatProfileSlice } from './slices/chat_profile'
 import { mockApi } from '../services/service'
 
 export const store = configureStore({
   reducer: {
     // Slices
     auth: authSlice.reducer,
+    user: userSlice.reducer,
+    chatProfile: chatProfileSlice.reducer,
     // RTK Query APIs
     [mockApi.reducerPath]: mockApi.reducer,
   },
