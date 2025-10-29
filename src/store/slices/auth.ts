7c570e682a23172be325a2d3b6805e8e2449add9
Raul <raulalfonzo66@gmail.com>
2025-10-29 10:05:49 -0400

    chore: modify the structure for the 'auth' slice

--- src/store/slices/auth.ts
+++ src/store/slices/auth.ts
@@ -1,30 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
 
 export interface AuthState {
   isLoggedIn: boolean
-  user: {
-    id: string
-    email: string
-  } | null
 }
 
 const initialState: AuthState = {
   isLoggedIn: false,
-  user: null,
 }
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<{ id: string; email: string }>) => {
+    login: (state) => {
       state.isLoggedIn = true
-      state.user = action.payload
     },
     logout: (state) => {
       state.isLoggedIn = false
-      state.user = null
     },
   },
 })
