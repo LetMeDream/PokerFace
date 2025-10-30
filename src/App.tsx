6a34b0643c6faecf7e2a8acaffeb33af2ed8ad23
Raul <raulalfonzo66@gmail.com>
2025-10-30 01:46:18 -0400

    chore (App.tsx): remove unused code in App.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,10 @@ import Sections from './components/liveChat/Sections'
 import Login from './pages/Login'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { useSelector } from 'react-redux'
 import DashboardPage from './pages/DashboardPage'
 import PrivateRoute from './layout/PrivateRoute'
-interface AuthState {
-  auth: {
-    isLoggedIn: boolean;
-  }
-}
 
 function App() {
-  const { isLoggedIn } = useSelector((state: AuthState) => state.auth);
-  console.log(isLoggedIn)
 
   return (
     <>
