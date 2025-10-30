d9599203585bda4fd862763eb3faedb6ab4251b9
Raul <raulalfonzo66@gmail.com>
2025-10-30 01:53:51 -0400

    chore (Chat.ts: MessagesProps): make props optional and add new one, in order to adapt type to the new Component's implementation

--- src/types/Chat.ts
+++ src/types/Chat.ts
@@ -1,15 +1,17 @@
 type ChatMessage = {
   type: string;
+  message_type?: string;
   content: string;
 };
 
 export type MessagesProps = {
   chatMessages: ChatMessage[];
-  isSending: boolean;
-  setIsSending: (isSending: boolean) => void;
-  setIsUserConected: (isUserConected: boolean) => void;
-  isUserConected: boolean;
-  chatBodyRef: React.RefObject<HTMLDivElement | null>;    
+  isSending?: boolean;
+  setIsSending?: (isSending: boolean) => void;
+  setIsUserConected?: (isUserConected: boolean) => void;
+  isUserConected?: boolean;
+  chatBodyRef?: React.RefObject<HTMLDivElement | null>; 
+  type?: 'guest' | 'agent';
 };
 
 export type FormValues = {
