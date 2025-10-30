c660632edaa7fc9c5227046d3aba8e432438bed8
Raul <raulalfonzo66@gmail.com>
2025-10-30 01:40:09 -0400

    refactor (logUserOut): unset 'base' on logUserOut

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,10 +1,12 @@
 import { logout } from "../store/slices/auth"
 import { clearUser } from "../store/slices/user"
 import { unsetChatProfile } from "../store/slices/chat_profile"
+import { unsetBase } from "../store/slices/base"
 import { store } from "../store/store"
 
 export const logUserOut = () => {
   store.dispatch(logout())
   store.dispatch(clearUser())
   store.dispatch(unsetChatProfile())
+  store.dispatch(unsetBase())
 }
