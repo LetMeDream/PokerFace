83b2a8710519c7915faaa2ae270900ac41dd09c7
Raul <raulalfonzo66@gmail.com>
2025-10-30 01:45:24 -0400

    style (ReceivedMessage): continue the styling for the ReceivedMessages componenet, used to display the conversations in the drawer's sidebar

--- src/components/Dashboard/ReceivedMessage.tsx
+++ src/components/Dashboard/ReceivedMessage.tsx
@@ -4,34 +4,38 @@ import { FaUserAstronaut } from "react-icons/fa6";
 const ReceivedMessage = ({
   avatarSrc,
   name,
-  messages
+  messages,
+  onClick
 }: {
   avatarSrc: string;
   name: string;
   messages: any;
+  onClick: () => void;
 }) => {
   const [imgError, setImgError] = useState(false);
   const lastMessage = messages?.length ? messages[messages.length - 1] : null;
 
   return (
-    <li className="flex-row items-center flex-nowrap group !rounded-sm border min-h-[80px]">
+    <li className="flex-row items-center flex-nowrap group !rounded-sm border min-h-[80px] max-w-full" onClick={onClick}>
       {/* Avatar */}
       {avatarSrc && !imgError ? (
-            <div className="rounded-l-sm rounded-r-none !basis-3/12 group-hover:!bg-slate-600">
+            /* Avatar image */
+            <div className="rounded-l-sm rounded-r-none !basis-3/12 group-hover:!bg-slate-600 self-stretch flex justify-center items-center">
               <div className="mask mask-squircle w-12">
                 <img src={avatarSrc} onError={() => setImgError(true)} />
               </div>
             </div>
           ) : (
+            /* Fallback avatar */
             <div className="rounded-l-sm self-stretch flex justify-center group-hover:!bg-slate-600">
               <FaUserAstronaut className="text-white w-12 h-12" />
             </div>
           )}
 
       {/* Content */}
-      <div className="content rounded-r-sm rounded-l-none flex flex-col items-start grow group-hover:!bg-slate-600 relative -left-[0.75px] self-stretch">
+      <div className="content rounded-r-sm rounded-l-none flex flex-col overflow-hidden items-start grow group-hover:!bg-slate-600 relative -left-[0.75px] self-stretch">
         <a className="!text-primary">{name}</a>
-        <span className="text-sm text-white">{lastMessage?.content}</span>
+        <span className="text-sm text-white break-words">{lastMessage?.content}</span>
       </div>
     </li>
   )
