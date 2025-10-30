1f97ea2e68aef773567e7c2e9085e941b88d76cb
Raul <raulalfonzo66@gmail.com>
2025-10-30 01:56:28 -0400

    feat (DashBoardPage): continue implementation of general logic inside DashboardPage

--- src/hooks/useDashboard.tsx
+++ src/hooks/useDashboard.tsx
@@ -1,7 +1,11 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
-const useDashboard = () => {
+import { useSelector } from "react-redux";
+import { setHasAutoOpened } from "../store/slices/base";
+import { useDispatch } from "react-redux";
 
+const useDashboard = () => {
+  /* Resizing Logic */
   const drawerButtonRef = useRef<HTMLLabelElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -14,10 +18,38 @@ const useDashboard = () => {
       containerRef.current.style.minHeight = `${viewportHeight - drawerButtonHeight}px`;
     }
   }, [isMobile, drawerButtonRef, containerRef])
+  /* Resizing Logic END */
+
+  /* Opening drawer automatically upon login in */
+  const { hasAutoOpened } = useSelector((state: any) => state.base);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!hasAutoOpened) {
+      setTimeout(() => {
+        const drawerCheckbox = document.getElementById('my-drawer-1') as HTMLInputElement;
+        if (drawerCheckbox) drawerCheckbox.checked = true;
+        dispatch(setHasAutoOpened(true));
+      }, 1500);
+    }
+  }, [hasAutoOpened, dispatch]);
+
+  const classnames = {
+    container: 'bg-secondary container !min-w-full md:min-h-screen',
+    drawerBtn: 'btn drawer-button bg-base-300 rounded-none absolute bottom-[0.5px] left-[0.25px] px-10 py-6',
+    searchInput: "input input-bordered w-full mb-4 caret-primary active:!ring-1 focus-within:!ring-1 focus-visible:!ring-1 focus:!ring-1 active:!outline-none focus-within:!outline-none focus-visible:!outline-none focus:!outline-none active:!border-none focus-within:!border-none focus-visible:!border-none focus:!border-none"
+  }
+
+  /* Search logic value */
+  const [searchValue, setSearchValue] = useState<string>('');
+
+
 
   return {
     drawerButtonRef,
-    containerRef
+    containerRef,
+    searchValue,
+    setSearchValue,
+    classnames
   }
 }
 
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,25 @@
-
-
+import Header from "../components/Dashboard/Header";
+import ReceivedMessage from "../components/Dashboard/ReceivedMessage";
+import useDashboard from "../hooks/useDashboard";
+import { allTickets } from "../constants/chat";
+import { setSelectedTicket } from "../store/slices/base";
+import { useDispatch } from "react-redux";
+import type { ChatTicket } from "../types/Slices";
+import Messages from "../components/liveChat/Messages/Messages";
+import { useSelector } from "react-redux";
+import { RiSearchLine, RiSettingsLine } from "react-icons/ri";
 
 export default function DashboardPage() {
+  const { drawerButtonRef, containerRef, searchValue, setSearchValue, classnames } = useDashboard();
+  const selectedTicket = useSelector((state: any) => state.base.selectedTicket);
 
-  /* Make .container 100vh, by calculating  */
+  const ticketsQty = allTickets.length;
+
+  const dispatch = useDispatch();
+
+  const handleTicketClick = (ticket: ChatTicket) => {
+    dispatch(setSelectedTicket(ticket));
+  };
 
   return (
     <>
@@ -12,66 +28,115 @@ export default function DashboardPage() {
         <div className="drawer-content">
           {/* Page content here */}
           <div 
-
-            className='bg-amber-50 container min-h-screen min-w-screen'
+            className={classnames.container}
+            ref={containerRef}
           >
-        
             {/* Header */}
-            <div className="navbar bg-base-100 shadow-sm">
-              <div className="navbar-start">
-                {/* <div className="dropdown">
-                  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                  </div>
-                  <ul
-                    tabIndex={-1}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li><a>Item 1</a></li>
-                    <li>
-                      <a>Parent</a>
-                      <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                      </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                  </ul>
-                </div> */}
-                <a className="btn btn-ghost text-xl">Logo</a>
-              </div>
-              {/* <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                  <li><a>Item 1</a></li>
-                  <li>
-                    <details>
-                      <summary>Parent</summary>
-                      <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                      </ul>
-                    </details>
-                  </li>
-                  <li><a>Item 3</a></li>
-                </ul>
-              </div> */}
-              <div className="navbar-end">
-                <a className="btn">Log out</a>
-              </div>
-            </div>
+            <Header />
 
-          <label htmlFor="my-drawer-1" className="btn drawer-button rounded-none absolute bottom-[0.5px] left-[0.25px]">Open drawer</label>
+            {/* Current Selected chat conversation */}
+            {selectedTicket ? (                
+              <AgentChat selectedTicket={selectedTicket} />
+            ) : (
+                <div className="flex flex-col items-center justify-center h-full text-secondary pt-6 text-white">
+                  <h1 className="!text-xl md:!text-3xl  font-bold mb-4">Selecciona una conversación</h1>
+                  <p className="text-lg text-center">Elige una conversación de la barra lateral para ver los mensajes aquí.</p>
+                </div>
+            )}
+
+
+            {/* Button for drawer */}
+            <label 
+              htmlFor="my-drawer-1" className={classnames.drawerBtn}
+              ref={drawerButtonRef}
+            >
+                Conversaciones <span className="badge bg-secondary">{ticketsQty}</span>
+            </label>
           </div>  
         </div>
+
+        {/* Sidebar */}
         <div className="drawer-side">
           <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          <ul className="menu bg-secondary min-h-full p-4 max-w-80">
             {/* Sidebar content here */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
+            <div className="caret-transparent mb-4">
+              <h2 className="caret-transparent">
+                Buscar:
+              </h2>
+              <input 
+                type="text" 
+                placeholder="Buscar..." 
+                className={classnames.searchInput} 
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+              />
+            </div>
+            {/* Message */}
+            <div className="flex flex-col gap-2 max-w-full">
+              {allTickets.filter(ticket => ticket.messages.length > 0)
+                .filter(ticket => ticket.nickname.toLowerCase().includes(searchValue.toLowerCase()))
+                .map((ticket) => (
+                  /* Mensajes de la Bandeja de conversaciones */
+                  <ReceivedMessage 
+                    key={ticket.chat_room_id}
+                    avatarSrc={ticket?.avatarSrc || ''}
+                    name={`${ticket.nickname}`}
+                    messages={ticket.messages}
+                    onClick={() => {
+                      handleTicketClick(ticket as any);
+                      const drawerCheckbox = document.getElementById('my-drawer-1') as HTMLInputElement;
+                      if (drawerCheckbox && drawerCheckbox.checked) {
+                        drawerCheckbox.checked = false; // Cierra el drawer al seleccionar un ticket
+                      }
+                    }}
+                  />
+                ))}
+            </div>
+
           </ul>
         </div>
       </div>
 
     </>
   );
+}
+
+/* Agent Chat */
+function AgentChat ({selectedTicket}: {selectedTicket: ChatTicket}) {
+  return (
+    <div>
+      {/* Placeholder for selected chat conversation */}
+      <div className="flex flex-col items-start justify-start h-full p-4">
+        {/* Placeholder for selected chat messages */}
+        <div className="flex flex-col text-secondary w-full items-center">
+          {/* Chat Body */}
+          <div className=" bg-cyan-50 rounded-xl max-w-[90vw] w-6xl py-5">
+              {/* Title and name */}
+              <div className="md:p-6 p-2 px-8 border-b border-gray-300 flex items-center gap-2 justify-between">
+                <p className="text-sm text-gray-600 md:text-end text-nowrap">Nombre del usuario: 
+                  <span className="font-medium text-gray-800 ml-1">
+                  {selectedTicket.nickname}
+                  </span>
+                </p>
+                {/* Icons for look for message, and settings */}
+                <div className="flex gap-2">
+                    <RiSearchLine className="w-5 md:w-6 md:h-6 text-indigo-700 hover:text-indigo-900 cursor-pointer" />
+                    <RiSettingsLine className="w-5 md:w-6 md:h-6 text-indigo-700 hover:text-indigo-900 cursor-pointer" />
+                </div>
+              </div>
+              {/* Messages Area */}
+              <div className="p-4 h-96 overflow-y-auto">
+                  <Messages
+                    chatMessages={selectedTicket.messages}
+                    type='agent'
+                  />
+              </div>
+              
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
