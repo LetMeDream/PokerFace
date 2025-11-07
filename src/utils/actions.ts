9617fab8331b3af9dd16689309e6c9ca06c11f6f
Raul <raulalfonzo66@gmail.com>
2025-11-07 15:44:02 -0400

    feat (logout): modify logUserOut function so that it now call to the server's api before being able to logout

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -3,21 +3,37 @@ import { clearUser, setUser } from "../store/slices/user"
 import { unsetChatProfile, setChatProfile } from "../store/slices/agent"
 import { unsetBase, setTickets } from "../store/slices/base"
 import { store } from "../store/store"
-import type { LoginSuccess } from "../services/service"
+import type { LoginSuccess } from "../types/Slices"
 import { mockApi } from "../services/service"
+import toast from "react-hot-toast"
+import { getApiErrorMessage } from "../utils/helpers"
 
-export const logUserOut = () => {
-  store.dispatch(logout())
-  store.dispatch(clearUser())
-  store.dispatch(unsetChatProfile())
-  store.dispatch(unsetBase())
+export const logUserOut = async () => {
+  const result = await store.dispatch(mockApi.endpoints.logout.initiate())
+  // Check for error in the API without typescript yelling at us
+  if ('error' in result && result.error) {
+    console.error("Logout failed", result.error);
+    const message = getApiErrorMessage(result);
+    toast.error(message);
+    return;
+  } else {
+    store.dispatch(logout())
+    store.dispatch(clearUser())
+    store.dispatch(unsetChatProfile())
+    store.dispatch(unsetBase())
+    toast.success("Logged out successfully")
+  }
+
 }
   
 export const setLoggedInUser = async (result: LoginSuccess) => {
-  const { user: userData, agent: agentData } = result;
+  const { user: userData, agent: agentData, token } = result;
   store.dispatch(setUser(userData))
   store.dispatch(setChatProfile(agentData))
-  store.dispatch(login())
+  store.dispatch(login({ token }))
+  /* 
+  TODO: Fetch initial data after login
+  */
   const res: any = await store.dispatch(mockApi.endpoints.getTickets.initiate())
   store.dispatch(setTickets(res?.data?.data ?? []))
 }
