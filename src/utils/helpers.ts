c3f37283d425b0c6555cbfaa0a1f7a1e283a4edf
Raul <raulalfonzo66@gmail.com>
2025-11-07 15:42:05 -0400

    chore: create the 'getApiErrorMessage' function to handle errors from the API

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,6 @@
 import type { AllTickets, NormalizedTickets } from "../types/Slices";
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query"
+import type { SerializedError } from "@reduxjs/toolkit"
 
 /**
  * Normaliza un array de tickets a { byId, allIds }
@@ -20,3 +22,26 @@ export const normalizeTickets = (tickets: AllTickets = []) => {
 
 /** Utility function that returns a Promise that resolves after a specified delay in milliseconds */
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+
+/* 
+* Function for getting error out of the RTK Query result without typescript fucking screaming at us 
+*/
+export const getApiErrorMessage = (result: any): string => {
+  // Type guard manual: ¿tiene 'status'? → FetchBaseQueryError
+      const isApiError = (error: any): error is FetchBaseQueryError => 
+        error && typeof error === 'object' && 'status' in error;
+  
+      let message = "Logout failed";
+  
+      if (isApiError(result.error)) {
+        // Seguro: tiene .data
+        const data = result.error.data as { detail?: string } | undefined;
+        message = data?.detail || message;
+      } else {
+        // Seguro: es SerializedError → tiene .message
+        const serialized = result.error as SerializedError;
+        message = serialized.message || message;
+      }
+      return message;
+}
