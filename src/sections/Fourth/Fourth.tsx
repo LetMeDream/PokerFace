b58b7f8dcaec46cd9c49f159fa18959fd7dd2281
Raul <raulalfonzo66@gmail.com>
2025-11-08 13:11:58 -0400

    feat: replace previous Images in the 'Fourth' section component with their new updated svg versions

--- src/sections/Fourth/Fourth.tsx
+++ src/sections/Fourth/Fourth.tsx
@@ -1,10 +1,12 @@
-import testimoniosdesktop from '../../assets/sections/fourth/testimonios-desktop.png'
-import testimonios3220 from '../../assets/sections/fourth/testimonios-3220.png'
+import testimoniosdesktop from '../../assets/sections/fourth/tribet juega desktop.svg'
+import testimonios3220 from '../../assets/sections/fourth/tribet juega 3220.svg'
+import testimoniosmobile from '../../assets/sections/fourth/tribet_juega_telefono.svg'
 
 const Fourth = () => {
   return (
     <div className=' overflow-hidden'>
-      <img src={testimoniosdesktop} alt="testimonios desktop" className=' block 2xl:hidden h-[200px] md:h-[unset] w-full' fetchPriority='high' />
+      <img src={testimoniosmobile} alt="testimonios mobile" className=' md:hidden' />
+      <img src={testimoniosdesktop} alt="testimonios desktop" className=' hidden md:block 2xl:hidden h-[200px] md:h-[unset] w-full' fetchPriority='high' />
       <img src={testimonios3220} alt="testimonios 3220" className=' hidden 2xl:block' />
     </div>
   )
