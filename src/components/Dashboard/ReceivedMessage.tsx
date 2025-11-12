0602e66902f32cb6b11042eb41959f3049272ef7
Raul <raulalfonzo66@gmail.com>
2025-11-12 19:53:23 -0400

    fix (ReceivedMessages): perform some little fixes to the component

--- src/components/Dashboard/ReceivedMessage.tsx
+++ src/components/Dashboard/ReceivedMessage.tsx
@@ -8,7 +8,8 @@ const ReceivedMessage = ({
   chatMessage: ReceivedChatMessage
   onClick: () => void;
 }) => {
-  const lastMessage = chatMessage?.messages?.length ? chatMessage.messages[chatMessage.messages.length - 1] : null;
+  const filteredMessages = chatMessage?.messages?.filter(msg => msg.sender_type !== 'system') ?? [];
+  const lastMessage = filteredMessages.length ? filteredMessages[filteredMessages.length - 1] : null;
 
   return (
     <li className="flex-row items-center flex-nowrap group !rounded-sm border min-h-[80px] max-w-full" onClick={onClick}>
@@ -21,7 +22,7 @@ const ReceivedMessage = ({
 
       {/* Content */}
       <div className="content rounded-r-sm rounded-l-none flex flex-col overflow-hidden items-start grow group-hover:!bg-slate-600 relative -left-[0.75px] self-stretch">
-        <a className="!text-primary">{chatMessage?.chat_user_info?.phone_number}</a>
+        <a className="!text-primary">{chatMessage?.chat_user_info?.phone_number || 'Missing Phone Number'}</a>
         <span className="text-sm text-white break-words">{lastMessage?.content}</span>
       </div>
     </li>
