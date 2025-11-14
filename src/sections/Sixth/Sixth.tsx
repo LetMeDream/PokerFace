71c65f768bf6d36b805267551fc2a5d7cc9aac50
Raul <raulalfonzo66@gmail.com>
2025-11-14 09:52:40 -0400

    style: add remaining icon for Sixth Section component

--- src/sections/Sixth/Sixth.tsx
+++ src/sections/Sixth/Sixth.tsx
@@ -1,12 +1,12 @@
 import './Sixth.css'
-import tribetClub from '../../assets/sections/sixth/tribet-club.png'
+import tribetClub from '../../assets/sections/sixth/tribetJuega.png'
 import instagram from '../../assets/sections/sixth/instagram.png'
 import whatssap from '../../assets/sections/sixth/whatssapp.png'
 
 const Sixth = () => {
   return (
     <section className="main-container pb-4 2xl:flex 2xl:flex-row-reverse 2xl:items-center md:pb-12 2xl:justify-center 2xl:py-14 caret-transparent 2xl:gap-6">
-      <img src={tribetClub} alt="Tribet Club" className='max-w-[300px] w-[70px] md:w-[160px] md:pt-6 mx-auto 2xl:mx-[0px] pt-3 pb-5 ' />
+      <img src={tribetClub} alt="Tribet Club" className='max-w-[300px] w-[140px] md:w-[240px] md:pt-6 md:pb-3 mx-auto 2xl:mx-[0px] pt-3 pb-5 ' />
 
       <div className="icons flex flex-col gap-2">
 
@@ -17,7 +17,7 @@ const Sixth = () => {
           </a>
         </div>
 
-        <div className='icon-container flex gap-2 items-center justify-center  relative max-w-[90vw] mx-auto md:gap-3 md:!max-w-max 2xl:py-0 2xl:px-0 2xl:mx-[unset]'>
+        <div className='icon-container flex gap-2 items-center justify-center  relative max-w-[90vw] mx-auto md:gap-3 md:!max-w-max 2xl:py-0 2xl:px-0 2xl:mx-[unset] pb-10 md:pb-4'>
           <img src={instagram} alt="Instagram" className=' max-w-[22px] relative' />
           <span className=' text-white relative montserrat-normal text-xs md:text-lg 2xl:text-2xl '>
             tribet.club_
