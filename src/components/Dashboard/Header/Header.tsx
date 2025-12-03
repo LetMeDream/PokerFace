e1eafb15cd338642678b163e852043d92f0b9c6b
Raul <raulalfonzo66@gmail.com>
2025-12-03 15:06:58 -0400

    feat (Header): add logic for unsetSelectedTicketId when clicking the header from the dashboard

--- src/components/Dashboard/Header/Header.tsx
+++ src/components/Dashboard/Header/Header.tsx
@@ -2,18 +2,30 @@ import { logUserOut } from "../../../utils/actions"
 import { useSelector } from "react-redux"
 import type { UserState } from "../../../types/Slices";
 import { Notifications } from "./Notifications";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { unsetSelectedTicketId } from "../../../store/slices/base";
+import { useDispatch } from "react-redux";
 
 const Header = () => {
   const user = useSelector((state: any) => state.user) as UserState;
   const fullName = `${user.first_name || ''} ${user.last_name || ''}`.trim(); 
   const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const handleOnClick = () => {
+    if (location.pathname !== '/dashboard') {
+      navigate('/dashboard');
+    } else {
+      dispatch(unsetSelectedTicketId());
+    }
+  }
 
   return (
     <div className="navbar bg-gray-600 shadow-sm">
       <div className="navbar-start">
 
-        <a className="btn btn-ghost text-lg !text-sky-50" onClick={() => navigate('/dashboard')}>Agente</a>
+        <a className="btn btn-ghost text-lg !text-sky-50" onClick={handleOnClick}>Agente</a>
       </div>
 
       <div className="navbar-end gap-4">
