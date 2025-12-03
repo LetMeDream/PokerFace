ab62783d81cd281fcece1493732d3972d6b98ca9
Raul <raulalfonzo66@gmail.com>
2025-12-03 01:23:44 -0400

    chore (Header): add redirect to dashboard from the Header

--- src/components/Dashboard/Header/Header.tsx
+++ src/components/Dashboard/Header/Header.tsx
@@ -2,16 +2,18 @@ import { logUserOut } from "../../../utils/actions"
 import { useSelector } from "react-redux"
 import type { UserState } from "../../../types/Slices";
 import { Notifications } from "./Notifications";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const user = useSelector((state: any) => state.user) as UserState;
   const fullName = `${user.first_name || ''} ${user.last_name || ''}`.trim(); 
+  const navigate = useNavigate();
 
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="navbar-start">
 
-        <a className="btn btn-ghost text-lg !text-sky-50">Agente</a>
+        <a className="btn btn-ghost text-lg !text-sky-50" onClick={() => navigate('/dashboard')}>Agente</a>
       </div>
 
       <div className="navbar-end gap-4">
