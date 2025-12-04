1360852a6b34ea1f531f9aa16007e847fdc3702c
Raul <raulalfonzo66@gmail.com>
2025-12-04 02:16:17 -0400

    feat (AllNotifications): add markup and logic for the component

--- src/pages/Dashboard/AllNotifications.tsx
+++ src/pages/Dashboard/AllNotifications.tsx
@@ -1,13 +1,22 @@
 import Header from '../../components/Dashboard/Header/Header'
 import { selectNotificationsArray } from '../../utils/selectors'
 import { useSelector } from 'react-redux'
+import Pagination from '../../components/Dashboard/Pagination'
+import usePagination from '../../hooks/usePagination'
+import { formatDistance } from 'date-fns'
+import { notificationStatusMessages } from '../../constants/chat'
 
 const AllNotifications = () => {
   const classnames = {
     container: 'bg-secondary container !min-w-full !min-h-[100dvh]',
   }
   const notifications = useSelector(selectNotificationsArray);
-  console.log(notifications.slice(0, 10))
+  const {
+    currentItems: paginatedNotifications,
+    totalPages,
+    goToPage,
+    currentPage
+  } = usePagination({elements: notifications, itemsPerPage: 10});
 
   return (
     <div className={classnames.container}>
@@ -15,17 +24,45 @@ const AllNotifications = () => {
       <Header />
 
       <div className="p-6">
-        {/* <div role="alert" className="alert alert-vertical sm:alert-horizontal max-w-2xl mx-auto">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info h-6 w-6 shrink-0">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-          </svg>
-          <div>
-            <h3 className="font-bold">¡Nueva notificación!</h3>
-            <div className="text-xs">{currentNotification?.message || ''}</div>
-          </div>
-          <button className="btn btn-sm" onClick={handleSeeNotification}>Marcar como leído</button>
-        </div> */}
+
+        <div className="overflow-x-auto bg-gray-600 rounded-t-2xl p-2 max-w-4xl mx-auto">
+          <table className="table table-sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Número</th>
+                <th
+
+                >Mensaje</th>
+                <th>Creada Hace</th>
+                <th>Tipo</th>
+                <th>Leído</th>
+
+              </tr>
+            </thead>
+            <tbody>
+
+              {paginatedNotifications.map((notification, index) => (
+                <tr key={notification.id} className={notification.is_read ? '' : 'bg-gray-400 font-bold'}>
+                  <th>{index + 1}</th>
+                  <td>{notification.id}</td>
+                  <td>{notification.message}</td>
+                  <td>{formatDistance(new Date(notification.created_at), new Date(), { addSuffix: true })}</td>
+                  <td>{notificationStatusMessages[notification.notification_type as keyof typeof notificationStatusMessages]}</td>
+                  <td>{notification.is_read ? 'Sí' : 'No'}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       
+        <div className="mt-4 flex justify-center">
+          <Pagination 
+            totalPages={totalPages}
+            currentPage={currentPage}
+            goToPage={goToPage}
+          />
+        </div>
 
       </div>
     </div>
