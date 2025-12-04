34c3d467af38f96b712835a1b615cddef2a6b76a
Raul <raulalfonzo66@gmail.com>
2025-12-03 20:02:37 -0400

    feat (AllNotifications): WIP. Start creating the component

--- src/pages/Dashboard/AllNotifications.tsx
+++ src/pages/Dashboard/AllNotifications.tsx
@@ -1,30 +1,13 @@
 import Header from '../../components/Dashboard/Header/Header'
+import { selectNotificationsArray } from '../../utils/selectors'
 import { useSelector } from 'react-redux'
-import { useMarkNotificationReadMutation } from '../../services/service'
-import toast from 'react-hot-toast'
-import { useNavigate } from 'react-router-dom'
 
 const AllNotifications = () => {
   const classnames = {
     container: 'bg-secondary container !min-w-full !min-h-[100dvh]',
   }
-  const currentNotification = useSelector((state: any) => state.agent.current_notification);
-
-  const navigate = useNavigate();
-  /* read notification individually */
-  const [markNotificationRead] = useMarkNotificationReadMutation();
-  const handleSeeNotification = async () => {
-    try {
-      await markNotificationRead({ notificationIds: [currentNotification?.id] }).unwrap()
-      toast.success('Notificación marcada como leída');
-      setTimeout(() => {
-        navigate('/dashboard');
-      }, 1000);
-    } catch (error) {
-      console.error('Failed to mark notification as read:', error);
-      toast.error('Error al marcar la notificación como leída');
-    }
-  }
+  const notifications = useSelector(selectNotificationsArray);
+  console.log(notifications.slice(0, 10))
 
   return (
     <div className={classnames.container}>
@@ -32,7 +15,7 @@ const AllNotifications = () => {
       <Header />
 
       <div className="p-6">
-        <div role="alert" className="alert alert-vertical sm:alert-horizontal max-w-2xl mx-auto">
+        {/* <div role="alert" className="alert alert-vertical sm:alert-horizontal max-w-2xl mx-auto">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info h-6 w-6 shrink-0">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
           </svg>
@@ -41,7 +24,9 @@ const AllNotifications = () => {
             <div className="text-xs">{currentNotification?.message || ''}</div>
           </div>
           <button className="btn btn-sm" onClick={handleSeeNotification}>Marcar como leído</button>
-        </div>
+        </div> */}
+      
+
       </div>
     </div>
   )
