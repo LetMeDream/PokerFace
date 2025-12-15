09e66b8a29be574401e2fd97827fd0eaab5cdc85
Raul <raulalfonzo66@gmail.com>
2025-12-15 13:05:12 -0400

    chore ('Chatear' Button): move the 'Chatear' button from the Fifth component into the Second one

--- src/sections/Fifth/Fifth.tsx
+++ src/sections/Fifth/Fifth.tsx
@@ -5,12 +5,6 @@ import './Fifth.css'
 
 const Fifth = () => {
 
-  const openChat = () => {
-    const chatBanner = document.getElementById('chat-banner');
-    if (chatBanner) {
-      chatBanner.click();
-    }
-  }
 
   const togglePlay = (videoId: string) => {
     const videoElement = document.getElementById(`video-${videoId}`) as HTMLVideoElement | null;
@@ -39,27 +33,8 @@ const Fifth = () => {
 
   return (
     <section className="caret-transparent overflow-hidden">
-      <div className=' bg-primary text-secondary text-[13px] montserrat-font uppercase pb-4 pt-2 px-10 text-center md:text-3xl md:px-20 md:pt-9 md:pb-11 2xl:py-12'>
-        <p className=' 2xl:max-w-[700px] 2xl:mx-auto'>
-          "Escribinos para crear tu usuario ahora, atención 24 horas"
-        </p>
-      </div>
       <div className="bg-black">
         <div className=' relative pt-12 flex flex-row justify-center 2xl:max-w-[900px] 2xl:mx-auto px-2'>  
-          {/* Chatear */}
-          <div className="absolute -top-4 left-1/2 -translate-y-1 md:-translate-y-6">
-            {/* <img src={chatear} alt="Chat Icon" className="chat-pulse max-w-[180px] md:max-w-[440px]" /> */}
-            <div>
-              <div 
-                className=" 
-                  absolute bg-[#18b720] hover:bg-[#0abb13] transition-colors duration-500 rounded-full px-36 py-2 chat-pulse 
-                  montserrat-font text-4xl uppercase right-1/2 top-2/12 cursor-pointer"
-                onClick={openChat}
-              >
-                  Chatear
-              </div>
-            </div>
-           </div>
           <div className=' flex-1 mb-3 flex flex-col items-center'>
             {/* VIDEO 1 */}
             <div className="video-container relative md:mt-10 cursor-pointer max-w-[45vw] w-[140px] md:w-[240px]">
--- src/sections/Second/Second.tsx
+++ src/sections/Second/Second.tsx
@@ -2,13 +2,48 @@ import './Second.css'
 import dekstop from '../../assets/sections/second/banner-second-web-juega365.webp'
 import bigercreen from '../../assets/sections/second/banner-second-3220-juega365.webp'
 
-const Second = () => {
+export const Second = () => {
+
+
+
   return (
-    <div className='bg-secondary'>
-      <img src={dekstop} alt="desktop" className='second-desktop-image block 2xl:hidden md:pt-2 2xl:pt-0 w-[100vw] relative -top-1 h-[140px] md:h-[unset]' />
-      <img src={bigercreen} alt="3220" className='second-3220-image hidden 2xl:block -mt-4' />
-    </div>
+    <>
+      <div className='bg-secondary relative'>
+        <img src={dekstop} alt="desktop" className='second-desktop-image block 2xl:hidden md:pt-2 2xl:pt-0 w-[100vw] relative -top-1 h-[140px] md:h-[unset]' />
+        <img src={bigercreen} alt="3220" className='second-3220-image hidden 2xl:block -mt-4' />
+      </div>
+      <Chatear />
+    </>
   )
 }
 
-export default Second
+const Chatear = () => {
+
+  const openChat = () => {
+    const chatBanner = document.getElementById('chat-banner');
+    if (chatBanner) {
+      chatBanner.click();
+    }
+  }
+
+  return (
+    <div className=' bg-primary text-secondary text-[13px] montserrat-font uppercase pb-2 pt-2 px-3 text-center md:text-3xl md:px-20 md:py-4 2xl:py-12'>
+      <p className=' 2xl:max-w-[700px] 2xl:mx-auto mb-2 md:mb-6'>
+        "Escribinos para crear tu usuario ahora, atención 24 horas"
+      </p>
+      {/* Chatear */}
+      <div className=" z-10">
+        {/* <img src={chatear} alt="Chat Icon" className="chat-pulse max-w-[180px] md:max-w-[440px]" /> */}
+        <div>
+          <div 
+            className=" 
+               bg-[#18b720] hover:bg-[#0abb13] transition-colors duration-500 rounded-full py-2 chat-pulse montserrat-font text-4xl uppercase cursor-pointer max-w-xs mx-auto"
+            onClick={openChat}
+          >
+              Chatear
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
