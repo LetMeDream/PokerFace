e9ec611e56f963001ab0abba7d005fd3260b3f11
Raul <raulalfonzo66@gmail.com>
2025-12-15 13:28:48 -0400

    feat (ReceivedMessages): implement feat for 'closing' and 'deleting' messages from the ReceivedMessage component

--- src/components/Dashboard/ReceivedMessage.tsx
+++ src/components/Dashboard/ReceivedMessage.tsx
@@ -1,9 +1,9 @@
 import { FaUserAstronaut } from "react-icons/fa6";
 import type { ReceivedChatMessage } from '../../types/Slices';
-import { formatDistance } from "date-fns";
-import { es } from 'date-fns/locale';
-import { useSelector } from "react-redux";
-import { selectNotificationsArray } from "../../utils/selectors";
+import { AiOutlineCheckCircle, AiOutlineUserDelete } from "react-icons/ai";
+import { useReceivedMessage, useUnassignAgent, useResolveAgent } from "../../hooks/useReceivedMessage";
+import Modal from "./Modal";
+
 
 const ReceivedMessage = ({
   chatMessage,
@@ -12,31 +12,73 @@ const ReceivedMessage = ({
   chatMessage: ReceivedChatMessage
   onClick: () => void;
 }) => {
-  const filteredMessages = chatMessage?.messages?.filter(msg => msg.sender_type !== 'system') ?? [];
-  const lastMessage = filteredMessages.length ? filteredMessages[filteredMessages.length - 1] : null;
+  const { isUnread, lastMessage, timeStamp } = useReceivedMessage({ chatMessage });
+  
+  // Unassign Agent Hook
+  const { handleShowUnassignTicketModal, unassignTicketModalId, closeBtnId, unassignAgentFromTicketHandler, isUnassigning } = useUnassignAgent({ chatMessage });
 
-  const notifications = useSelector(selectNotificationsArray);
+  // Resolve Chat Hook
+  const { handleShowResolveTicketModal, resolveTicketModalId, closeTicketModalId, isResolving, resolveChatHandler } = useResolveAgent({ chatMessage });
 
-  const isUnread = notifications?.some(n => n.chat_room_id === lastMessage.chat_room_id && !n.is_read);
 
   return (
-    <li className={`flex-row items-center flex-nowrap group !rounded-sm border min-h-[80px] md:min-w-[240px]! max-w-full ${isUnread ? 'border-orange-400 border-2' : ''}`} onClick={onClick}>
-      {/* Avatar */}
+    <>
+      <li className={`flex-row items-center flex-nowrap group !rounded-sm border min-h-[80px] md:min-w-[240px]! max-w-full ${isUnread ? 'border-orange-400 border-2' : ''}`} onClick={onClick}>
+        {/* Avatar */}
 
-      <div className="rounded-l-sm self-stretch flex justify-center group-hover:!bg-slate-600">
-        <FaUserAstronaut className="text-white w-12 h-12" />
-      </div>
+        <div className="rounded-l-sm self-stretch flex justify-center group-hover:!bg-slate-600">
+          <FaUserAstronaut className="text-white w-12 h-12" />
+        </div>
 
 
-      {/* Content */}
-      <div className="content rounded-r-sm rounded-l-none flex flex-col overflow-hidden items-start grow group-hover:!bg-slate-600 relative -left-[0.75px] self-stretch">
-        <a className="!text-primary">{chatMessage?.chat_user_info?.phone_number || 'Missing Phone Number'}</a>
-        <span className="text-sm text-white break-words">{lastMessage?.content}</span>
-        <span className="absolute bottom-0.5 text-gray-400 right-2 text-[10px] bg-secondary rounded-sm px-1">
-          {lastMessage ? formatDistance(new Date(lastMessage.created_at), new Date(), { addSuffix: true, locale: es }) : ''}
-        </span>
-      </div>
-    </li>
+        {/* Content */}
+        <div className="content rounded-r-sm rounded-l-none flex flex-col overflow-hidden items-start grow group-hover:!bg-slate-600 relative -left-[0.75px] self-stretch">
+          <a className="!text-primary text-xs ">{chatMessage?.chat_user_info?.phone_number || 'Missing Phone Number'}</a>
+          <span className="text-sm text-white break-words">{lastMessage?.content}</span>
+          <span className="absolute bottom-0.5 text-gray-400 right-2 text-[10px] bg-secondary rounded-sm px-1">
+            {timeStamp}
+          </span>
+          <div 
+            className="flex absolute top-2 right-2 group opacity-0 group-hover:opacity-100 gap-0.5 bg-secondary rounded-sm p-0.5"
+            onClick={(event) => {
+              // stop bubbling to parent onClick
+              event?.stopPropagation();
+            }}
+          >
+            <AiOutlineCheckCircle 
+              className="  text-gray-400 w-[18px] h-[18px] hover:text-green-500/70 cursor-pointer transition-colors" 
+              title="Marcar Chat como Resuelto"  
+              onClick={handleShowResolveTicketModal}
+            />
+            <AiOutlineUserDelete 
+              className=" text-gray-400 w-[18px] h-[18px] hover:text-red-500/70 cursor-pointer transition-colors" 
+              title="Desasignar Agente del Chat"  
+              onClick={handleShowUnassignTicketModal}
+            />
+          </div>
+        </div>
+      </li>
+      {/* Unassign Modal */}
+      <Modal
+        acceptFunction={unassignAgentFromTicketHandler}
+        isLoading={isUnassigning}
+        type='info'
+        message='¿Desea desasignarse del ticket y volver a la bandeja de entrada?'
+        btnMessage='Desasignar'
+        id={unassignTicketModalId}
+        closeBtnId={closeBtnId}
+      />
+      {/* Resolve Modal */}
+      <Modal
+        acceptFunction={resolveChatHandler}
+        isLoading={isResolving}
+        type='info'
+        message='¿Desea marcar el ticket como resuelto y cerrarlo?'
+        btnMessage='Marcar como Resuelto'
+        id={resolveTicketModalId}
+        closeBtnId={closeTicketModalId}
+      />
+    </>
   )
 }
 
--- src/hooks/useReceivedMessage.tsx
+++ src/hooks/useReceivedMessage.tsx
@@ -3,8 +3,13 @@ import { useSelector } from "react-redux";
 import { selectNotificationsArray } from "../utils/selectors";
 import { formatDistance } from "date-fns";
 import { es } from 'date-fns/locale';
+import { useUnassignAgentMutation, useGetAssignedChatsQuery, useGetWaitingChatsQuery, useResolveChatMutation } from "../services/service";
+import { useDispatch } from "react-redux";
+import { setAssignedChats } from "../store/slices/agent";
+import { setTickets, unsetSelectedTicketId } from "../store/slices/base";
+import { toast } from "react-hot-toast";
 
-const useReceivedMessage = ({
+export const useReceivedMessage = ({
   chatMessage,
 }:{
   chatMessage: ReceivedChatMessage
@@ -24,4 +29,109 @@ const useReceivedMessage = ({
   }
 }
 
-export default useReceivedMessage
+// * Hook for handling logic for 'Unassigning' Agent 
+export const useUnassignAgent = ({
+  chatMessage,
+}:{
+  chatMessage: ReceivedChatMessage
+}) => {
+
+  const handleShowUnassignTicketModal = () => {
+    const dialog = document.getElementById(unassignTicketModalId) as HTMLDialogElement | null;
+    if (dialog) dialog.showModal();
+  }
+
+  const unassignTicketModalId = `unassign_ticket_modal_${chatMessage.id}`;
+  const closeBtnId = `unassign_ticket_close_btn_${chatMessage.id}`;
+
+  const [unassignAgent, { isLoading: isUnassigning }] = useUnassignAgentMutation();
+  const { refetch: refetchAssignedChats } = useGetAssignedChatsQuery()
+  const { refetch: refetchWaitingChats } = useGetWaitingChatsQuery();
+  
+  const dispatch = useDispatch();
+  const unassignAgentFromTicketHandler = async () => {
+    try {
+      if (chatMessage) {
+        await unassignAgent({ ticketId: chatMessage.id }).unwrap();
+        const assignedChatsResults = await refetchAssignedChats();
+        const data = (assignedChatsResults as any)?.data;
+        if (data && data.chats) {
+          // Update assigned chats in the store
+          dispatch(setAssignedChats(data.chats));
+        }
+        const waitingChatsResults = await refetchWaitingChats();
+        const waitingData = (waitingChatsResults as any)?.data;
+        if (waitingData && waitingData.chats) {
+          // Update waiting chats in the store
+          dispatch(setTickets(waitingData.chats));
+        }
+
+        dispatch(unsetSelectedTicketId());
+      }
+    } catch (error) {
+      console.error('Error desasignando el ticket:', error);
+      toast.error('Error desasignando el ticket. Por favor, inténtelo de nuevo.');
+    } finally {
+      const dialog = document.getElementById(unassignTicketModalId) as HTMLDialogElement | null;
+      if (dialog) dialog.close();
+    }
+
+  
+  }
+
+  return {
+    handleShowUnassignTicketModal,
+    unassignTicketModalId,
+    closeBtnId,
+    isUnassigning,
+    unassignAgentFromTicketHandler
+  }
+}
+
+// * Hook for handling logic for 'Resolving' Agent 
+export const useResolveAgent = ({
+  chatMessage,
+}:{
+  chatMessage: ReceivedChatMessage
+}) => {
+
+  const dispatch = useDispatch();
+  const resolveTicketModalId = `close_ticket_modal_${chatMessage.id}`;
+  const closeTicketModalId = `close_ticket_close_btn_${chatMessage.id}`;
+  // Show Resolve Ticket Modal
+  const handleShowResolveTicketModal = () => {
+    const dialog = document.getElementById(resolveTicketModalId) as HTMLDialogElement | null;
+    if (dialog) dialog.showModal();
+  }
+  const [resolveChatMutation, { isLoading: isResolving }] = useResolveChatMutation();
+  const { refetch: refetchAssignedChats } = useGetAssignedChatsQuery()
+  const resolveChatHandler = async () => {
+    try {
+      if (chatMessage) {
+        await resolveChatMutation({ ticketId: chatMessage.id }).unwrap();
+        const result = await refetchAssignedChats();
+          const data = (result as any)?.data;
+          if (data && data.chats) {
+            // Update assigned chats in the store
+            dispatch(setAssignedChats(data.chats));
+          }
+      }
+    } catch (error) {
+      console.error('Error resolviendo el ticket:', error);
+      toast.error('Error resolviendo el ticket. Por favor, inténtelo de nuevo.');    
+    } finally {
+      const dialog = document.getElementById(resolveTicketModalId) as HTMLDialogElement | null;
+      if (dialog) dialog.close();
+    }
+  }
+
+
+
+  return {
+    resolveTicketModalId,
+    closeTicketModalId,
+    handleShowResolveTicketModal,
+    isResolving,
+    resolveChatHandler
+  }
+}
