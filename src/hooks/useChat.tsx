b1e097e53389c4b05d081f989c7d4fb0d79b7375
Raul <raulalfonzo66@gmail.com>
2025-12-15 13:09:28 -0400

    style (useChat): add border to chat to improve visibility

--- src/hooks/useChat.tsx
+++ src/hooks/useChat.tsx
@@ -23,11 +23,11 @@ const useChat = () => {
 
   const classnames = {
     // 'overflow-hidden' is crucial so the chat body is hidden when collapsed
-    widgetWrapper: 'md:left-10 fixed md:rounded-tr-3xl text-sm bottom-0 w-[100dvw] md:max-w-[80vw] md:max-w-[310px] md:w-[310px] caret-transparent overflow-hidden !caret-transparent',
+    widgetWrapper: 'md:left-10 fixed text-sm bottom-0 w-[100dvw] md:max-w-[80vw] md:max-w-[310px] md:w-[310px] caret-transparent overflow-hidden !caret-transparent',
     // Base classes for the chat body; includes height/opacity transitions
     chatBody: 'bg-white md:h-[450px] shadow-lg flex flex-col transition-all duration-300 ease-in-out ',
     // Clickable banner at the top
-    banner: 'bg-primary text-secondary montserrat regular px-3 py-2 cursor-pointer flex items-center justify-between gap-2 transition'
+    banner: 'bg-primary text-secondary md:rounded-tr-3xl montserrat regular px-3 py-2 cursor-pointer flex items-center justify-between gap-2 transition border! border-black!'
   };
 
   const toggleChat = () => {
