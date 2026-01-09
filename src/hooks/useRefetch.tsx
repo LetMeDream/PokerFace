cf791f17f1ee6735a649805d420aeab270c0c7f8
Raul <raulalfonzo66@gmail.com>
2026-01-08 20:17:53 -0400

    fix (useRefetch/useRefetchGuestChatStatus): modify the custom hook so that it always refetches guest chat status and messages on mount

--- src/hooks/useRefetch.tsx
+++ src/hooks/useRefetch.tsx
@@ -63,34 +63,27 @@ export const useRefetchNotifications = () => {
 
 /* Refetches guest chat status */
 export const useRefetchGuestChatStatus = () => {
-  const { id: chat_room_id, session_id: sessionId, status } = useSelector((state: any) => state.guest);
-  const skipQuery = !sessionId || status === 'closed' || status === 'resolved';
-  const { data: guestChatStatusData } = useGetGuestChatStatusQuery<any>({ chat_room_id }, { skip: skipQuery });
+  const { id: chat_room_id, /* session_id: sessionId, */ status } = useSelector((state: any) => state.guest);
+  // Ejecuta la query si hay sessionId y el status no es 'closed' ni 'resolved'
+  const skipQuery = status === 'closed' || status === 'resolved';
+  const { data: guestChatStatusData } = useGetGuestChatStatusQuery<any>({ chat_room_id }, { skip: skipQuery, refetchOnMountOrArgChange: true });
   const dispatch = useDispatch();
-  const currentMessages = useSelector((state: any) => state.guest.messages);
 
   useEffect(() => {
-    // console.log(guestChatStatusData)
     if (!guestChatStatusData) return;
 
     let settableMessages = guestChatStatusData?.message_history || [];
-    // Map the messages to match the guest slice structure
     settableMessages = settableMessages.filter((msg: any) => msg.sender_type !== 'system')
-    .map((msg: any) => ({
-      type: msg.sender_type === 'user' ? 'guest' : (msg.sender_type === 'agent' || msg.sender_type === 'system') ? 'agent' : 'unknown',
-      content: msg.content
-    }));
-    // console.log(settableMessages)
+      .map((msg: any) => ({
+        type: msg.sender_type === 'user' ? 'guest' : (msg.sender_type === 'agent' || msg.sender_type === 'system') ? 'agent' : 'unknown',
+        content: msg.content
+      }));
     dispatch(setGuestStatus(guestChatStatusData?.status || ''));
-    if (currentMessages.length === 0 && settableMessages.length > 0) {
+    if (settableMessages.length > 0) {
+      dispatch(setGuestMessages([]))
       dispatch(setGuestMessages(settableMessages));
     }
-
-
-    // setGuestMessages(guestChatStatusData?.message_history || []);
   }, [guestChatStatusData, dispatch]);
 
-  return {
-    
-  }
+  return {};
 }
